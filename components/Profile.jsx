a1cb3300bb3fcf3c58b6f2eec9d95699d263bc3b
Nabeel-Sadiq <sadiqnabeel4@gmail.com>
2023-08-30 22:19:00 +0500

    Edit Functionality Done

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,6 +1,27 @@
-const Profile = () => {
+import PromptCard from "./PromptCard"
+
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile </span>
+      </h1>
+      <p className="desc tex-left">
+        {desc}
+      </p>
+      <div className="mt-10 prompt_layout">
+        {console.log(data.post)}
+      {data.map((post) => (
+        <PromptCard
+         key={post._id}
+         post={post}
+         handleEdit={() => handleEdit && handleEdit(post)}
+         handleDelete={() => handleDelete && handleDelete(post)}
+        />
+      ))}
+    </div>
+    </section>
   )
 }
 
